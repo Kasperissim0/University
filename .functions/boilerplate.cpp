3b4b00bc33f540acaf97f833cfe19f6f4a9b058f
Kasperissim0 <dankor42q@gmail.com>
2025-12-09 16:21:29 +0100

    tweaked boilerplate

--- .functions/boilerplate.cpp
+++ .functions/boilerplate.cpp
@@ -2,17 +2,16 @@
 #include "screen.h" // adds clearScreen()          + <iomanip> and <cstdlib>
 #include "wait.h"   // adds wait(unsigned seconds) + <thread> and <chrono>
 #include "colors.h" // adds color modifiers Color::FG/BG/ATTR::(Bright::)Modify
-// use "clang++ -std=c++23 x.cpp -I../../../.functions-link -o x" for to compile successfully
+// use "clang++ -std=c++23 x.cpp -I../../../.functions-link -o x" to compile successfully
 
 using namespace std;
 
-int main (int argc, char** args) { // if (argn != ) return 1;
+int main (int argc, char** args) { // if (argc != ) return 1;
 	char goAgain;
 	
-	do {
-		clearScreen();
-		cout << r  << i << Color::FG::Bright::BLUE << "hello " <<  flush;
-		wait(2); cout << r  << Color::FG::Bright::YELLOW << "there";
+	do { clearScreen();
+		cout << r  << i << Color::FG::Bright::BLUE << "hello " <<  flush; wait(2); 
+		cout << Color::FG::Bright::YELLOW << "there";
 	} while (cout << r  << b << "\n\n  " << u << "➡︎ Go Again?" 
 								<< r << b << " (" << green << "y"<< black 
 								<< b << '/' << red << 'n' << r << "): "
