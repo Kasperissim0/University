5644f79d4e320bd857ecfe6bc404def210dfc770
Kasperissim0 <dankor42q@gmail.com>
2026-03-01 09:56:36 +0100

    refactoring

--- 2025w/.homework/11th/task7/minsweeper.cpp
+++ 2025w/.homework/11th/task7/minsweeper.cpp
@@ -1,4 +1,4 @@
-// § Preamble
+//§ Preamble
   #include <chrono>
   #include <cstddef>
   #include <cstdlib>
@@ -7,6 +7,7 @@
   #include <thread>
   #include "random.hpp"
   using namespace std;
+//.
 
 //? Source(s):  https://moodle.univie.ac.at/mod/extserver/view.php?id=19368923
 //?             https://minesweeperhub.com/basic-minesweeper-rules/
@@ -15,26 +16,6 @@
   
 constexpr size_t DIMENSION = 9;
 
-int getUserInput(const int from, const int to, const string message = "") {
-  string input, errorMessage = "❌ Invalid Input Entered, Try Again";
-  bool validInput = false;
-  int numericalInput;
-  do {
-    if (!message.empty()) cout << message << endl;
-    cout << "  ➡︎ Input Range (" << from << "-" << to << "): ";
-    getline(cin, input);
-
-    try {
-      numericalInput = stoi(input);
-      validInput = true;
-      break;
-    } catch (...) {}
-    cout << errorMessage << endl;
-    this_thread::sleep_for(chrono::seconds(2));
-    system("clear");
-  } while (!validInput);
-  return numericalInput;
-}
 void printBoard(bool board[DIMENSION][DIMENSION]) {
   auto v = "|", h = "---", x = "X", q = "?", s = " ";
   char index = 'A';
@@ -59,9 +40,31 @@ void printBoard(bool board[DIMENSION][DIMENSION]) {
     cout << h;
   cout << endl;
 }
+int getUserInput(const int from, const int to, const string message = "") {
+  string input, errorMessage = "❌ Invalid Input Entered, Try Again";
+  bool validInput = false; int numericalInput;
+
+  do {
+    if (!message.empty()) cout << message << endl;
+    cout << "  ➡︎ Input Range (" << from << "-" << to << "): ";
+    getline(cin, input);
+
+    try {
+      numericalInput = stoi(input);
+      validInput = true;
+      break;
+    } catch (...) {}
+    cout << errorMessage << endl;
+    this_thread::sleep_for(chrono::seconds(2));
+    system("clear");
+  } while (!validInput);
+  
+  return numericalInput;
+}
 
 int main(int n, char* args[]) {
-  bool playBoard[DIMENSION][DIMENSION] = {}; size_t xCoordinate, yCoordinate;
+  bool playBoard[DIMENSION][DIMENSION] = {}; 
+  size_t xCoordinate, yCoordinate;
 
   for (size_t a = 0; a < DIMENSION; ++a)
     for (size_t b = 0; b < DIMENSION; ++b)
@@ -72,4 +75,5 @@ int main(int n, char* args[]) {
   cout << endl << "Choose Row/Column:" << endl;
 
   // } while ();
+  return 0;
 }
--- 2025w/.homework/12th/task12/custom-iterator.cpp
+++ 2025w/.homework/12th/task12/custom-iterator.cpp
@@ -7,6 +7,23 @@
 //.
 
 //? Source(s):  https://moodle.univie.ac.at/mod/extserver/view.php?id=19368923
+/* 
+  Iterators are used to traverse lists of objects in a given order. 
+  They are often used to formulate algorithms (e.g. sorting algorithms) independently of the internal representation of the objects or the list.
+  Write three classes, Object, List, and Iterator, to provide the following functionality: Object is a proxy for any class. 
+  For test purposes, this class should have at least one print method that enables the objects to be uniquely identified. 
+  List is to manage objects in a dynamic array. 
+  The list should have add and insert methods, which can insert an object at the end and at the beginning of the list, respectively. 
+  Iterator should contain the ++ and -- operators, which are used to move forward and backward in the list, respectively, and the * (dereferencing) operator, which returns the current object. 
+  An iterator that refers to the first element of a list is returned by calling the begin() method of the List class. 
+  Assuming that the list l is filled with the four objects 'a', 'b', 'c' and 'd', e.g. B. the following piece of program will deliver the listed results:
+      Iterator it{l.begin()};
+      (*it).print(); //Output of 'a';
+      it++;
+      (*it).print(); //Output of 'b';
+      it--;
+      (*it).print(); //Output of 'a';
+*/
 
 namespace Data {
   template <typename T>
