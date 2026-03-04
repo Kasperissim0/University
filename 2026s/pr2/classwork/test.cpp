a388ef666b7619659b6729ecef2cbbf211f85c44
Kasperissim0 <dankor42q@gmail.com>
2026-03-04 18:29:59 +0100

    classwork

---
+++ 2026s/pr2/classwork/test.cpp
@@ -0,0 +1,35 @@
+#include "screen.hpp"  // adds clearScreen()
+#include "wait.hpp"    // adds wait(unsigned seconds)
+#include "colors.hpp"  // adds color modifiers Color::FG/BG/ATTR::(Bright::)Modify
+#include "random.hpp"  // adds "gen" variable with type generation methods
+#include "outputs.hpp" // adds << overloads for common classes (vectors, maps, ...)
+//* use "clang++ -std=c++23 x.cpp -I../../../.functions-link -o x" to compile
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main (int argc, char** args) { // if (argc != ) return 1;
+	char goAgain;
+  
+	do { clearScreen();
+  	try {
+      // cout << "🚧 Attempting To Delete " << gen.getFile() << endl; 
+      gen.deleteFile();
+      // cout << "✅ Succesfully Deleted " << gen.getFile() << endl;
+    } catch (const runtime_error& e) { cerr << "❌ ERROR: " << e.what() << endl; }
+		cout << Color::FG::Bright::YELLOW << "List Of Random Chars: " << r << endl; wait(1);
+    cout << gen.setAmount(9).writeValues<char>(' ', '~').readValues();
+    try {
+      // cout << "🚧 Attempting To Delete " << gen.getFile() << endl; 
+      gen.deleteFile();
+      // cout << "✅ Succesfully Deleted " << gen.getFile() << endl;
+    } catch (const runtime_error& e) { cerr << "❌ ERROR: " << e.what() << endl; } 
+	} while (cout << r  << b << "\n\n  " << u << "➡︎ Go Again?"
+								<< r << b << " (" << green << "y"<< black
+								<< b << '/' << red << 'n' << r << "): "
+								&& cin >> goAgain && goAgain == 'y');
+	clearScreen();
+	return 0;
+}
